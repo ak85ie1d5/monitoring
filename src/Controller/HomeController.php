fe652a509ead9bdecc1ffdcad35b94f2eee660bb
Jeremy Spaeth <jspaeth@prevor.com>
2020-11-28 20:44:52 +0100

    *analyse* page send **CURL** request on all sites and save code status in databases (monitoring.status).

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Status;
 use App\Entity\Website;
 use App\Repository\WebsiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +22,36 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'websites' => $websites
         ]);
+    }
+
+    /**
+     * @Route("/websites/analyze", name="analyze")
+     */
+    public function analyze(WebsiteRepository $repository, EntityManagerInterface $manager)
+    {
+        // Récupèrer tous nos sites
+        $websites = $repository->findAll();
+
+        // Récupère le status actuel de chacun
+        foreach ($websites as $key => $site) {
+            $url = $site->getUrl();
+            $handle = curl_init($url);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($handle);
+            $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            curl_close($handle);
+
+            // Créer une nouvelle entité Status et l'enregistrer
+            $status = new Status();
+            $status->setCode($code)
+                ->setReportedAt(new \DateTime())
+                ->setWebsite($site);
+            $manager->persist($status);
+        }
+        $manager->flush();
+
+        // Se rediriger vers notre route "home"
+        return $this->redirectToRoute('home');
     }
 
     /**
