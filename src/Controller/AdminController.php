5c3c79f4f7fd85ca309a2a79508be8d9c1f2dda0
Jeremy Spaeth <jspaeth@prevor.com>
2020-11-29 22:13:01 +0100

    Add form to enter new website with datas validation befor submitting.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Website;
+use App\Form\WebsiteType;
 use App\Repository\WebsiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +21,27 @@ class AdminController extends AbstractController
         $websites = $websiteRepository->findAll();
         return $this->render('admin/index.html.twig', [
             'websites' => $websites
+        ]);
+    }
+
+    /**
+     * @Route("/admin/new", name="admin_new")
+     */
+    public function new(Request $request, EntityManagerInterface $manager)
+    {
+        $website = new Website();
+        $form = $this->createForm(WebsiteType::class, $website);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($website);
+            $manager->flush();
+            $this->addFlash('success', 'Site web ajouté avec succès');
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('admin/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Entity/Website.php
+++ src/Entity/Website.php
@@ -6,6 +6,7 @@ use App\Repository\WebsiteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=WebsiteRepository::class)
@@ -21,11 +22,20 @@ class Website
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Url(
+     *     message="The url {{ value }} is not a valid url",
+     * )
      */
     private $url;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *     min = 4,
+     *     max = 255,
+     *     minMessage = "Your site name must be at least {{ limit }} characters long",
+     *     maxMessage = "Your site name cannot be longer than {{ limit }} characters"
+     * )
      */
     private $name;
 
